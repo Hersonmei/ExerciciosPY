41359d068bccdc1a4ffa1e6becf8c81166f9dcdf
Hersonmei <herson.me@hotmail.com>
2021-04-21 10:15:19 -0300

    Inicial

---
+++ Python ThiadoDV/exercíciod4.py
@@ -0,0 +1,8 @@
+# Crie uma função que recebe o nome do usuário e mande um alô para ele.
+def alo(nome):
+    print('Alô, ' + nome)
+
+alo('Alex')
+alo('Igor')
+
+# Crie uma função que escreva 9 quadrantes (como se fosse um jogo da velha)

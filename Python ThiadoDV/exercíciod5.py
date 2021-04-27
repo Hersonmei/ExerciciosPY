caff31cdc601ce54fdadc1f3b6893c15eb518002
Hersonmei <herson.me@hotmail.com>
2021-04-27 14:21:37 -0300

    Finalização exercício cálculo IMC

--- Python ThiadoDV/exercíciod5.py
+++ Python ThiadoDV/exercíciod5.py
@@ -1,18 +1,27 @@
+#Calculadora para IMC
+#Primeiro solicitando o input das informações pelo usuário
+peso = int(input('Digite o seu peso: '))
+altura = float(input('Digite a sua altura: ' ).replace(",", "."))
+
+#Função de cálculo do IMC
 def imc(peso, altura):
     return peso / (altura * altura)
-        
-    if a < 18.5:
-        print('Abaixo do peso')
-    if 18.5 < a < 24.9:
-        print('Peso normal')
-    if 25 < a < 29.9:
-        print('Sobrepeso')
-    if 30 < a < 34.9:
-        print('Obesidade I')
-    if 35 < a < 39.9:
-        print('Obesidade II')
-    if a > 40:
-        print('Obesidade III')
 
-a = imc(80, 1.80)
-print(a)
+#Atribuição dos valores do IMC a variável b
+result = imc(peso, altura)
+
+b = result
+
+#Classificação de acordo com o resultado do IMC e impressão.
+if b < 18.5:
+    print('Abaixo do peso')
+elif 18.5 < b < 24.9:
+    print('Peso normal')
+elif 25 < b < 29.9:
+    print('Sobrepeso')
+elif 30 < b < 34.9:
+    print('Obesidade I')
+elif 35 < b < 39.9:
+    print('Obesidade II')
+elif b > 40:
+    print('Obesidade III')

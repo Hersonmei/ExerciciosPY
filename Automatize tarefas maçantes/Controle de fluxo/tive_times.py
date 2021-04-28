253f1d5dab2324962f07a6d98212c28348022adf
Hersonmei <herson.me@hotmail.com>
2021-04-28 15:27:44 -0300

    while

---
+++ Automatize tarefas maçantes/Controle de fluxo/tive_times.py
@@ -0,0 +1,10 @@
+print('My name is')
+for i in range(5):
+    print('Jimmy Five times (' + str(i) +')')
+
+# Aqui podemos observar que o while consegue fazer o mesmo que o for, no entanto o for é mais conciso.
+
+#i = 0
+#while i < 5:
+    #print('Jimmy Five times (' + str(i) +')')
+    #i = i + 1

b270ef779152d0d0aceef9146cbf9addacc424ca
Hersonmei <herson.me@hotmail.com>
2021-04-27 21:26:46 -0300

    Finalização dos 3 exercícios propostos.

--- Python ThiadoDV/exercíciod5.py
+++ Python ThiadoDV/exercíciod5.py
@@ -58,3 +58,22 @@ def maior_numero(num1, num2):
 
 resultado = maior_numero(num1, num2)
 print(resultado)
+
+# Esse código primeiro escolhe um número aleatório de 1 a 3, depois a outra função vai receber um número inserido pelo usuário e vai mostrar se o usuário advinhou ou não.
+import random
+
+def aleatorio():
+    return random.randint(1,3)
+
+number = aleatorio()
+
+
+def palpite(palp):
+    if number == palp:
+        print('Parabéns! Você acertou o número.')
+    elif number != palp:
+        print('O número era ' + str(number) + ' você errou o palpite.')
+
+number_palpite = int(input('Digite um número: '))
+palp = number_palpite
+palpite(palp)

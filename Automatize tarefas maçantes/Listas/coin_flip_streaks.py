72a6a3a3c5eeb5c6c7ef2156aee6ab7bf7151d88
Hersonmei <herson.me@hotmail.com>
2021-05-14 21:58:32 -0300

    birthdays

--- Automatize tarefas maçantes/Listas/coin_flip_streaks.py
+++ Automatize tarefas maçantes/Listas/coin_flip_streaks.py
@@ -6,13 +6,13 @@ for experimentNumber in range(10000):
     # Code that creats a list of 100 'heads' or 'tails' values
     randomize = random.randint(0,1)
     if randomize == 0:
-        numberOfStreaks.append('head')
+        numberOfStreaks.append(0)
     if randomize == 1:
-        numberOfStreaks.append('tail')
+        numberOfStreaks.append(1)
 
 
 count_dups = [sum(1 for _ in group) for _, group in groupby(numberOfStreaks)]
-print(count_dups)
+print(count_dups.count(6))
 
     # Code that cheks is there is a streak of 6 heads or tails in a row.
 #print('Chance of streak: %s%%' % (numberOfStreaks / 100))

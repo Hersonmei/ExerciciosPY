3032aeaae3c03dc56ff4271b291b9a5d9349e728
Hersonmei <herson.me@hotmail.com>
2021-05-15 20:00:48 -0300

    setdefault

---
+++ Automatize tarefas maçantes/Dicionários/characterCount.py
@@ -0,0 +1,9 @@
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+
+count = {}
+
+for chacarter in message:
+    count.setdefault(chacarter, 0)
+    count[chacarter] = count[chacarter] +1
+
+print(count)

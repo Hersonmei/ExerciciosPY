ddab7aa141d1e68994ccab04561a31d0fa9333d9
Hersonmei <herson.me@hotmail.com>
2021-06-02 23:09:34 -0300

    Início do jogo

---
+++ Learn python with games/guess.py
@@ -0,0 +1,11 @@
+import random
+
+guessesTaken = 0
+
+print('Hello! What is your name?')
+myName = input()
+
+number = random.randint(1,20)
+print('Well, ' + myName + ', I  am thinking of a number between 1 and 20')
+
+for guessesTaken in range(6):

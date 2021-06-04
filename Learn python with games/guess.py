2f7d76f5b4bbe069e58f1d90e135f81ad6b9c811
Hersonmei <herson.me@hotmail.com>
2021-06-03 22:16:28 -0300

    Término do jogo e início da minha construção

--- Learn python with games/guess.py
+++ Learn python with games/guess.py
@@ -6,6 +6,24 @@ print('Hello! What is your name?')
 myName = input()
 
 number = random.randint(1,20)
-print('Well, ' + myName + ', I  am thinking of a number between 1 and 20')
+print('Well, ' + myName + ', I  am thinking of a number between 1 and 20.')
 
 for guessesTaken in range(6):
+    print('Take a guess.')
+    guess = input()
+    guess = int(guess)
+
+    if guess < number:
+        print('Seu palpite é muito baixo')
+    if guess > number:
+        print('Seu palpite é muito alto')
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken)
+    print('Good job, ' + myName + '! Você acertou meu número em ' + guessesTaken + ' palpites!')
+
+if guess != number:
+    number = str(number)
+    print('Não. O número que eu estava pensando era ' + number + '.')

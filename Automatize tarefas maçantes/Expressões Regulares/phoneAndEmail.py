e1711142fa3dc4672530a6c5ecf981bc0411d51b
Hersonmei <herson.me@hotmail.com>
2021-06-05 18:15:05 -0300

    Finalizando o projeto

--- Automatize tarefas maçantes/Expressões Regulares/phoneAndEmail.py
+++ Automatize tarefas maçantes/Expressões Regulares/phoneAndEmail.py
@@ -21,7 +21,21 @@ emailRegex = re.compile(r'''(
     )''', re.VERBOSE)
 
 #Encontra correspondências no texto do clipboard.
+text = str(pyperclip.paste())
 
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
 
 #Copia os resultados para o clipboard.
-
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found')

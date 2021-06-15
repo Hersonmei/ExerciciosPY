43602b44fcfa90788ae52774ff4d773cb2a41fdb
Hersonmei <herson.me@hotmail.com>
2021-06-14 21:42:01 -0300

    Finalização do primeiro programa com sucesso!!

--- Learn python with games/dragon copy.py
+++ Learn python with games/dragon copy.py
@@ -6,31 +6,37 @@ def introducao():
 print()
 
 def escolha_jogador():
-    player_choose = []
-
+    player_choose = ''
     while player_choose != '1' and player_choose != '2':
         print('Qual caverna você escolhe? (1 ou 2)')
         player_choose = input()
-        
+    
+    return player_choose
 
 def caverna():
-    dragon_escolhido = random.randint(1,2)
+    print('Você vai entrando na caverna..')
+    time.sleep(3)
+    print('Ela está bastante escura e perigosa..')
+    time.sleep(3)
+    print('Você encontra uma criatura monstruosa e..')
+    time.sleep(3)
+    print('')
+
+    dragon_escolhido = random.randint(1, 2)
     return dragon_escolhido
 
 # Botar dentro de uma função, para poder colocar o time e o suspense...
 def jogo():
         escolha = escolha_jogador()
-        dragão = caverna()
+        dragão = str(caverna())
         if escolha == dragão:
             print('Que alívio... Você entrou na caverna do Dragão Gentil e ele lhe entregou todo o tesouro! Parabéns!')
-        else:
+        if escolha != dragão:
             print('Teeente correrrr!!! Você caiu na caverna do Dragão Faminto! Fuja!!')
 
-continuar = []
-while continuar != 's' and continuar != 'n':
+continuar = ''
+while continuar == 's' or continuar != 'n':
     introducao()
-    escolha_jogador()
-    caverna()
     jogo()
     print('Você deseja continuar jogando? (s ou n)')
     continuar = input()
